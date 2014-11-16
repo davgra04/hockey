e008a32bf865889b6e008499618ec142659f8ce8
devgru <davgra04@gmail.com>
2014-11-15 23:16:35 -0700

    Modified comments

--- hockey/main.cpp
+++ hockey/main.cpp
@@ -1,7 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2014)
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, math, and strings
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
